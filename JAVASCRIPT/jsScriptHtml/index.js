6aa470e1a389c61c2e08c004ec4eb70c81518078
Glayson.Santos <glaysonbrennod.santos@gmail.com>
2025-02-09 08:22:49 -0300

    exercicio1

---
+++ JAVASCRIPT/jsScriptHtml/index.js
@@ -0,0 +1,11 @@
+let nome = "brenno"
+console.log("Olá, mundo!")
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+// window.alert(nome)
+// nome = window.prompt("Insire o seu nome: ")
+// let confirmacao = window.confirm("Confirma essa ação?")
+
+
